ae256d485d3c2c362b1f393f9bc65aff5a322643
Ivan Epou <ivanepou92@gmail.com>
2022-06-30 11:53:22 -0700

    The put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+* main: entry point
+*
+* Return: always zero on success
+*
+* 
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
