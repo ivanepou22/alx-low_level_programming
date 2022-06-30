0b3414eaf2aeb2b07b4af11f8d36951752e1870d
Ivan Epou <ivanepou92@gmail.com>
2022-06-30 11:30:43 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/*
+/**
  *
  * main - Prints quote
  *
  * Return: zero on success
  *
  */
-
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
