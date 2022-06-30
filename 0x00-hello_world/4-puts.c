dfff51750378d1c1d7bbb66c581c7bd434bf904e
Ivan Epou <ivanepou92@gmail.com>
2022-06-30 11:23:07 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 
-/**
+/*
  *
  * main - Prints quote
  *
- *
- *
  * Return: zero on success
  *
- *     
- *
- *
-*/
+ */
 
-int main(void)
-
-{
-	
-     puts("\"Programming is like building a multilingual puzzle");
-
-     return (0);
-
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
