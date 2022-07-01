a7363f46f824b08fa0bcaf2b5332207cc221b00a
Ivan Epou <ivanepou92@gmail.com>
2022-07-01 13:46:11 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main- entry point
+*Description: Alphabets
+*Return: zero on success
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf(num);
+		num++;
+	}
+	printf('\n');
+	return (0);
+}
