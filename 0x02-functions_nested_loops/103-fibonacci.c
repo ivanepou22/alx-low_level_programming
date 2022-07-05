a633f59d02b30ad9653a37383bafce9d179f3e4d
Ivan Epou <ivanepou92@gmail.com>
2022-07-05 12:37:07 -0700

    fibonacci nums

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - print sum of all even fibonacci numbers under 4x10^6
+*Description: main - print sum of all even fibonacci numbers under 4x10^6
+*Return: Nothing.
+*/
+int main(void)
+{
+	unsigned long x, y, z, sum;
+
+	z = 0;
+	x = 0;
+	y = 1;
+	sum = 0;
+
+	while (z < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+
+		if (z & 2 == 0)
+			sum += z;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
