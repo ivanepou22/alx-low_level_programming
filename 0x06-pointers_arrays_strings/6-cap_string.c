5ae88566199738ea55a068ce905cb9d7ad936ef2
Ivan Epou <ivanepou92@gmail.com>
2022-07-13 14:22:26 -0700

    capitalize the words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,21 +8,20 @@
 
 char *cap_string(char *s)
 {
-	int count, c;
+	int count, i;
 
 	count = 0;
 
-	int letters[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
-
 	while (*(s + count) != '\0')
 	{
-		for (c = 0; c < 13; c++)
+		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == letters[c])
+			if (*(s + count) == words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
