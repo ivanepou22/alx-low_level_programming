2bc8dec870acacbb686d367df5a03cb1786dbb61
Ivan Epou <ivanepou92@gmail.com>
2022-07-14 01:21:16 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point.
+* Description: program prints a[2] = 98, followed by a new line.
+* Return: zero on success
+*/
+
+int main(void)
+{
+	int n;
+
+	int a[5];
+
+	int *p;
+
+	a[2] = 1024;
+
+	p = &n;
+
+	p[5] = 98;
+
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
