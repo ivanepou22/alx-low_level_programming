8011fc33d24d14ba3129f23781b96dfcdf8f8bdf
Ivan Epou <ivanepou92@gmail.com>
2022-07-20 08:33:11 -0700

    Find the Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - return string length
+*@s: string point
+*Return: recursion
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+*pal - palindrome
+*@s: pointer to string
+*@l: position
+*Return: boolena
+*/
+
+int pal(char *s, int l)
+{
+	if (l < 1)
+		return (1);
+	if (*s == *(s + l))
+		return (p1(s + 1, l - 2));
+	return (0);
+}
+
+/**
+* is_palindrome - palindrome
+* @s: pointer to string
+* Return: recursion
+*/
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	return (pal(s, len - 1));
+}
