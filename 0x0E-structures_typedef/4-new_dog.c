fe7357b16eb915e8a9042222ae3b4228e38b2c08
Ivan Epou <ivanepou92@gmail.com>
2022-08-01 19:38:27 +0300

    create dog object

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - creates dog object
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog object, NULL if fails
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+
+	if (hobbes == NULL)
+		return (NULL);
+
+
+	return (hobbes);
+}
