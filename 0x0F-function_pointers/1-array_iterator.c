f0ec33e52d908b87773361055f4869316df2d01f
Ivan Epou <ivanepou92@gmail.com>
2022-08-03 18:00:43 +0300

    Execute a function given as a paramater

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-array_iterator.c
+ * Author: Brian Bassey
+ */
+
+#include "function_pointers.h"
+
+/**
+ * array_iterator -executes a function given as a parameter
+ * on each element of an array
+ * @array: input array
+ * @size: number of elements to loop through
+ * @action: action to be executed on each element
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	if (action && array)
+		while (size--)
+			action(*(array++));
+
+}
