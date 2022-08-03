0a23f5968beddca156e7a9fc23dd8a8f05b5bd9c
Ivan Epou <ivanepou92@gmail.com>
2022-08-03 17:48:55 +0300

    Print Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
-*/
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: callback function that prints name
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!f || !name)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,10 @@
-#include <stddef.h>
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+#include <stdio.h>
 #include <stdlib.h>
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /*FUNC_POINTERS*/
