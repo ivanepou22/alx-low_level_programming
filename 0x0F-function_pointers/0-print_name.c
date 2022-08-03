ac6a290a10f508a38a10a41972fc0bc28dff59fc
Ivan Epou <ivanepou92@gmail.com>
2022-08-03 07:26:11 -0700

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
