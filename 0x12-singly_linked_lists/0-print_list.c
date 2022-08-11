7ac18ce0f1ffb562bfd5f45ce43236772b351496
Ivan Epou <ivanepou92@gmail.com>
2022-08-11 08:36:22 -0700

    Add print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - print all elements of a linked list
+ * @h: pointer to linked list
+ * Return: number of nodes in linked list
+ **/
+
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	printf("[%u] %s\n", h->len, h->str);
+
+	if (h->next != NULL)
+		return (print_list(h->next) + 1);
+	return (1);
+}
