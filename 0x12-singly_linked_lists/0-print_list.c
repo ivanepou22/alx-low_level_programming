7223abfa35d6a1a4d13e35ebf7986b9237205e46
Ivan Epou <ivanepou92@gmail.com>
2022-08-11 18:43:14 +0300

    Add print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,6 @@
  * @h: pointer to linked list
  * Return: number of nodes in linked list
  **/
-
 size_t print_list(const list_t *h)
 {
 	if (h == NULL)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,10 +10,10 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -26,7 +26,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 int _putchar(char c);
 
 #endif
-
