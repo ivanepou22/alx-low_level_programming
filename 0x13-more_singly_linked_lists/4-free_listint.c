8f53e5145fcffe1fc75f83f9ab5c2394a34f586c
Ivan Epou <ivanepou92@gmail.com>
2022-08-16 16:47:18 +0300

    Free a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,9 @@
-/*
- *  * File: 4-free_listint.c
- *   * 
- *    */
-
 #include "lists.h"
 
 /**
- *  * free_listint - Frees a listint_t list.
- *   * @head: A pointer to the head of the listint_t list to be freed.
- *    */
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
+*/
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
