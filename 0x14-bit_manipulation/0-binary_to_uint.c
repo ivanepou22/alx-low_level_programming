08db38cb45a75b6163e2a463a13d92f80621aa5a
Ivan Epou <ivanepou92@gmail.com>
2022-08-18 22:08:35 +0300

    Convert a string binary number into a unsigned in  @b: binary string

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * power - returns 2 ^ y
- * @c: only calculate if c != 0
- * @y: exponent value
- * Return: 2 ^ y or 0 if fail
- */
-int power(char c, int y)
-{
-	if (c == '0')
-		return (0);
-	else if (y == 0)
-		return (1);
-	else if (y == 1)
-		return (2);
-	return (2 * power(c, y - 1));
-}
-/**
- * binary - converts a string binary number into a unsigned int
- * @b: binary string
- * @num: pointer to number where value is stored
- * Return: decimal as unsigned int
- */
-unsigned int binary(const char *b, unsigned int *num)
-{
-	int index;
-
-	if (b[1] == '\0')
-	{
-		*num = b[0] - '0';
-		return (1);
-	}
-	index = binary(b + 1, num);
-	*num +=  power(b[0], index);
-	return (index + 1);
-}
-
-/**
- * binary_to_uint - converts a string binary number into a unsigned int
- * @b: binary string
- * Return: decimal as unsigned int
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, i;
+	unsigned int ui;
+	int len, base_two;
 
-	num = 0;
-
-	if (b == NULL || b[0] == '\0')
+	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '1' && b[i] != '0')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	binary(b, &num);
-	return (num);
+	return (ui);
 }
