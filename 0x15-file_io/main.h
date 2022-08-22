6de4a983a3c7d188f49aefe3dac8522a6b6c5452
Ivan Epou <ivanepou92@gmail.com>
2022-08-22 21:48:54 +0300

    Modify main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,30 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdarg.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-/* prototypes for ELF */
-void errors(char *msg);
-void pentry(void);
-void ptype(char *buffer);
-void pos(char *buffer);
-void pversion(char *buffer);
-void pABIversion(char *buffer);
-void pdata(char *buffer);
-void pmagic(char *buffer);
-void pclass(char *buffer);
-void ptitle(char *title);
-int main(int argc, char **argv);
-
 #endif
+
